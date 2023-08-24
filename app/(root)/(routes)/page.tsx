4b2f8ad33be42d38748099777b353937c16d81fe
HugoLc <hugolindoso.c@gmail.com>
2023-08-24 10:29:14 -0300

    feat(home): cria os cards e filtro por categoria e texto no inpu

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,13 +1,60 @@
 import Categories from "@/components/categories";
+import { Companions } from "@/components/companions";
 import SearchInput from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
 
-export default async function RootPage() {
+interface RootPageProps {
+  searchParams: {
+    // no next 13 todo componente de pagina tem essa prop
+    categoryId: string;
+    name: string;
+  };
+}
+
+export default async function RootPage({ searchParams }: RootPageProps) {
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+  /* const data2 = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name, // so é possível por que no model do prisma habilitamos a busca pelo text e habilitamos fulltext para o name
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true, // pega as mensagens da relação
+        },
+      },
+    },
+  }); */
+
   const categories = await prismadb.category.findMany({});
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={data} />
     </div>
   );
 }
